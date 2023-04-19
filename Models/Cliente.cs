44f3f0bf1e030a4e6fd9a9aa90528977c131341d
Hugo Vinícius Valentim Damasceno <hugo.damasceno@animaeducacao.com.br>
2023-04-19 09:49:37 -0300

    First commit

---
+++ Models/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpskillingGama.Models
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            this.id = Guid.NewGuid().ToString();
+        }
+
+        public Cliente(string userId)
+        {
+            this.id = userId;
+        }
+
+        private string id;
+        public string Id
+        {
+            get
+            {
+                return this.id;
+            }
+        }
+
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
